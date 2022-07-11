1928f5b7239e87ceb8bc87a785e0beff67e293ca
blitzkz23 <blitzaldykazu@gmail.com>
2022-07-11 09:49:43 +0700

    Refactor class component on header into function

--- src/components/NoteHeader.js
+++ src/components/NoteHeader.js
@@ -1,30 +1,14 @@
 import React from "react";
 
-class NoteHeader extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.onSearchChangeEventHandler =
-      this.onSearchChangeEventHandler.bind(this);
-  }
-
-  onSearchChangeEventHandler(event) {
-    event.preventDefault();
-    this.props.searchNote(event.target.value);
-  }
-
-  render() {
-    return (
-      <div className="note-app__header">
-        <img src="./colordots.svg" alt="color dots" width="80" />
-        <input
-          type="text"
-          placeholder="Cari catatan..."
-          onChange={this.onSearchChangeEventHandler}
-        ></input>
-      </div>
-    );
-  }
+export default function NoteHeader({ searchNote }) {
+  return (
+    <div className="note-app__header">
+      <img src="./colordots.svg" alt="color dots" width="80" />
+      <input
+        type="text"
+        placeholder="Cari catatan..."
+        onChange={(event) => searchNote(event.target.value)}
+      ></input>
+    </div>
+  );
 }
-
-export default NoteHeader;
