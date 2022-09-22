6b8dec26205022cfa41ab7416c84fe1fade5a2d0
blitzkz23 <blitzaldykazu@gmail.com>
2022-09-22 20:01:44 +0700

    feat: add error handler page for 404 not found route

--- src/components/NoteApp.js
+++ src/components/NoteApp.js
@@ -3,6 +3,7 @@ import AddPage from "../pages/AddPage";
 import HomePage from "../pages/HomePage";
 import { Routes, Route } from "react-router-dom";
 import NoteHeader from "./NoteHeader";
+import NotFound from "../pages/NotFound";
 
 export default function NoteApp() {
   return (
@@ -14,6 +15,7 @@ export default function NoteApp() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/add" element={<AddPage />} />
+          <Route path="/*" element={<NotFound />} />
         </Routes>
       </main>
     </div>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <div className="note-app__not-found">
+      <img src="./error404.svg" alt="Page not found" width="400" />
+      <p>Halaman tidak ditemukan</p>
+    </div>
+  );
+}
