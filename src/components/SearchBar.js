5c1263c14730598a3540680b6c745ddebd9731d2
blitzkz23 <blitzaldykazu@gmail.com>
2022-09-22 20:16:37 +0700

    feat: add react icon to search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,17 +1,24 @@
 import React from "react";
 import { PropTypes } from "prop-types";
+import { FiSearch } from "react-icons/fi";
 
 export default function SearchBar({ keyword, keywordChange, searchNote }) {
   return (
-    <input
-      type="text"
-      placeholder="Cari catatan..."
-      value={keyword}
-      onChange={(event) => {
-        searchNote(event.target.value);
-        keywordChange(event.target.value);
-      }}
-    ></input>
+    <div className="note-search__wrapper">
+      <div className="note-search__icon">
+        <FiSearch />
+      </div>
+      <input
+        className="note-search__input"
+        type="text"
+        placeholder="Cari catatan..."
+        value={keyword}
+        onChange={(event) => {
+          searchNote(event.target.value);
+          keywordChange(event.target.value);
+        }}
+      ></input>
+    </div>
   );
 }
 
